eb99d025c17905eacc1d56ab8f5305ac84dba4bd
Rajkishor Thakur <rajkishort596@gmail.com>
2025-11-05 21:18:15 +0530

    Initialized Client Side with Basic Setup.

---
+++ client/src/layouts/RootLayout.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import Container from "../components/Container/Container";
+import Header from "../components/Header/Header";
+
+const RootLayout = () => {
+  return (
+    <Container>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-1 relative px-4 lg:px-6 xl:px-31 bg-white overflow-y-auto">
+          <Outlet />
+        </main>
+      </div>
+    </Container>
+  );
+};
+
+export default RootLayout;
